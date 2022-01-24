5dc8301ba82a01edadf79fa3256a94419605647e
JoonHyuk <jh83370@naver.com>
2022-01-25 00:18:35 +0900

    requset 스코프와 프록시

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -16,14 +16,15 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider; //MyLogger를 찾아볼 수 있는, DL을 할 수 있는 애가 주입됨.
+    private final MyLogger myLogger;   //실제 myLogger가 아닌 가짜 myLogger. -> 실제로 매소드를 호출할 때,그 때 실제가 동작된다.
 
     @RequestMapping("log-demo")
     @ResponseBody   //뷰 화면 없이 문자열을 반환하고 싶을 때는 @ResponseBody 사용.
     public String logDemo(HttpServletRequest request){   //HttpServletRequest(자바에서 제공하는 표준 서블릿 규약)로 고객 요청 정보를 받을 수 있음.
         String requestURL = request.getRequestURI().toString();   //고객이 어떤 URL로 요청했는지 알 수 있음.
-        MyLogger myLogger = myLoggerProvider.getObject();    //필요한 시점에 myLogger를 받음.(HTTP리퀘스트가 발생한 이후여서 괜찮음)
-        myLogger.setRequestURL(requestURL);
+
+        System.out.println("myLogger = " + myLogger.getClass());
+        myLogger.setRequestURL(requestURL);   //이때 가짜가 아닌 진짜 myLogger가 동작함. 
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
